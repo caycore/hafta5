2fece04615a2ff72683b5be5a79c73cf90aaefaa
lfabl <met2net3@gmail.com>
2023-02-18 20:38:07 +0300

    Feature: Function parameters.

--- script.js
+++ script.js
@@ -13,3 +13,27 @@ function hesapla(type) {
         sonucText = Number(sayi1.value) - Number(sayi2.value);
     }
 }
+
+function test(ilksayi, ikincisayi, ucuncusayi, dorduncusayi) {
+    var sonuc = 0;
+
+    if(ilksayi) {
+        sonuc += ilksayi;
+    }
+
+    if(ikincisayi) {
+        sonuc += ikincisayi;
+    }
+
+    if(ucuncusayi) {
+        sonuc += ucuncusayi;
+    }
+
+    if(dorduncusayi) {
+        sonuc += dorduncusayi;
+    }
+
+    alert(sonuc);
+}
+
+test(0, 3, 1);
